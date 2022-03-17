dbecbd32261302b0ebfbb88f0e996d24d4ddbb73
Ahmet Akta <ahmadomegaman@gmail.com>
2022-03-17 17:32:23 +0300

    frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,125 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('wh2.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('wh2.png') }}">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{asset('loginpage/css/style.css')}}">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Login</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+							<div class="text w-100">
+								<h2>Welcome to login</h2>
+								<p>Don't have an account?</p>
+								<a href="{{route('register')}}" class="btn btn-white btn-outline-white">Sign Up</a>
+							</div>
+			      </div>
+						<div class="login-wrap p-4 p-lg-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Sign In</h3>
+			      		</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="https://www.linkedin.com/in/ahmet-akta/" class="social-icon d-flex align-items-center justify-content-center"><i class="fa fa-linkedin" aria-hidden="true"></i> </a>
+										<a href="https://twitter.com/ahmd_akta" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter" ></span></a>
+									</p>
+								</div>
+			      	</div>
+                      <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group mb-3">
+			      			<label class="label" for="name">Email</label>
+                              <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email"  required autocomplete="email" autofocus>
+
+                              @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+			      		</div>
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+                      <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+
+                      @error('password')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+		            </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+		            </div>
+		            <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+										</label>
+									</div>
+									<div class="w-50 text-md-right">
+										<a href="#">Forgot Password</a>
+									</div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+    <section class="footer">
+        <!-- Footer -->
+        <footer class="text-center text-white" style="background-color:#cb5464;">
+          <div class="container p-4 pb-0">
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3">Create Quiz for free</span>
+                <a type="button" href="{{route('register')}}" class="btn btn-outline-light btn-rounded" >
+                  Sign up!
+                </a>
+              </p>
+            </section>
+          </div>
+          <div class="text-center p-3" style="background-color: #cb5464;">
+            © 2022
+            <a class="text-white" href="https://twitter.com/ahmd_akta" target="blank">AHMED AKTA</a>
+          </div>
+        </footer>
+        <!-- Footer -->
+      </section>
+
+	<script src="{{asset('loginpage/js/jquery.min.js')}}"></script>
+  <script src="{{asset('loginpage/js/popper.js')}}"></script>
+  <script src="{{asset('loginpage/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('loginpage/js/main.js')}}"></script>
+
+	</body>
+</html>
+
+
+
+{{-- ***************************************************************** --}}
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +191,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,201 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+  <head>
+
+
+  	<title>Register</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('wh2.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('wh2.png') }}">
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{asset('loginpage/css/style.css')}}">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Register</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+							<div class="text w-100">
+								<h2>Welcome to Register</h2>
+								<p>have account? Login</p>
+								<a href="{{route('login')}}" class="btn btn-white btn-outline-white">Login</a>
+							</div>
+			      </div>
+						<div class="login-wrap p-4 p-lg-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Register</h3>
+			      		</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="https://www.linkedin.com/in/ahmet-akta/" class="social-icon d-flex align-items-center justify-content-center"><i class="fa fa-linkedin" aria-hidden="true"></i> </a>
+										<a href="https://twitter.com/ahmd_akta" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+									</p>
+								</div>
+			      	</div>
+							<form action="#" class="signin-form">
+			      		<div class="form-group mb-3">
+			      			<label class="label" for="name">Name</label>
+			      			<input type="text" class="form-control" placeholder="Username" required>
+			      		</div>
+                          <div class="form-group mb-3">
+                            <label class="label" for="name">Email</label>
+                            <input type="email" class="form-control" placeholder="Email" required>
+                        </div>
+		            <div class="form-group mb-3">
+		            	<label class="label" for="password">Password</label>
+		              <input type="password" class="form-control" placeholder="Password" required>
+		            </div>
+                    <div class="form-group mb-3">
+		            	<label class="label" for="password">Confirm Password</label>
+		              <input type="password" class="form-control" placeholder="Confirm Password" required>
+		            </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary submit px-3">Register</button>
+		            </div>
+		            <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+										</label>
+									</div>
+									<div class="w-50 text-md-right">
+										<a href="#">Forgot Password</a>
+									</div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+    <section class="footer">
+        <!-- Footer -->
+        <footer class="text-center text-white" style="background-color:#cb5464;">
+          <div class="container p-4 pb-0">
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3">Create Quiz for free</span>
+                <a type="button" href="{{route('register')}}" class="btn btn-outline-light btn-rounded" >
+                  Sign up!
+                </a>
+              </p>
+            </section>
+          </div>
+          <div class="text-center p-3" style="background-color: #cb5464;">
+            © 2022
+            <a class="text-white" href="https://twitter.com/ahmd_akta" target="blank">AHMED AKTA</a>
+          </div>
+        </footer>
+        <!-- Footer -->
+      </section>
+
+	<script src="{{asset('loginpage/js/jquery.min.js')}}"></script>
+  <script src="{{asset('loginpage/js/popper.js')}}"></script>
+  <script src="{{asset('loginpage/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('loginpage/js/main.js')}}"></script>
+
+	</body>
+</html>
+
+
+
+{{-- ***************************************************************** --}}
+
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
+
+
+
+
+
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -74,4 +271,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
