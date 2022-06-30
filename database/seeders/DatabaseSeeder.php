e74fb97c2c91b1ca409391213a4f86c8a05e4448
Ahmet Akta <ahmadomegaman@gmail.com>
2022-07-01 02:58:38 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,70 +21,19 @@ class DatabaseSeeder extends Seeder
          \App\Models\User::create([
              'name' => 'ahmed',
              'user_name' => 'ahmed',
-             'email'=> 'ahmed@gmail.com',
-             'role'=> 2,
-             'password' => '$2y$10$c8vEZwYV.gRnTqvE6ipLPuIbNZC/e5Q/eWlsM5xeH9tPhNIxO8npS', // asdasdasd
-         ]);
-         \App\Models\User::create([
-             'name' => 'admin',
-             'user_name' => 'admin',
-             'email'=> 'admin@gmail.com',
+             'email'=> 'ahmdekta@gmail.com',
              'role'=> 1,
-             'password' => '$2y$10$c8vEZwYV.gRnTqvE6ipLPuIbNZC/e5Q/eWlsM5xeH9tPhNIxO8npS', // asdasdasd
+             'password' => '$2y$10$k7a7O1WrOa3abqqiIfS8TObCHDpdPxulthyR0nXZz4yGjGzAdC0xe',
          ]);
-         \App\Models\User::create([
-            'name' => 'abuhmed',
-            'user_name'=>'abuhmed',
-            'role'=> 2,
-            'email'=> 'abuhmed@gmail.com',
-            'password' => '$2y$10$c8vEZwYV.gRnTqvE6ipLPuIbNZC/e5Q/eWlsM5xeH9tPhNIxO8npS', // asdasdasd
-        ]);
         \App\Models\Profile::create([
             'id' => '1',
             'user_id'=> '1',
-            'phone_number'=> '41232141',
+            'phone_number'=> '',
             'about'=> 'i am ahmed i am using quizcatch',
             'gender'=> 'Male',
             'photo'=> 'media/photo.jpeg',
-            'date_of_birth'=> '10.10.2000',
-            'education'=> '',
+            'date_of_birth'=> '17.08.2003',
+            'education'=> 'sabahattin zaim university',
         ]);
-        \App\Models\Profile::create([
-            'id' => '2',
-            'user_id'=> '2',
-            'phone_number'=> '41232141',
-            'about'=> 'i am admin i am using quizcatch',
-            'gender'=> 'Male',
-            'photo'=> 'media/photo2.jpeg',
-            'date_of_birth'=> '10.10.2000',
-            'education'=> '',
-        ]);
-        \App\Models\Profile::create([
-            'id' => '3',
-            'user_id'=> '3',
-            'phone_number'=> '41232141',
-            'about'=> 'i am abuhmed i am using quizcatch',
-            'gender'=> 'Male',
-            'photo'=> 'media/photo2.jpeg',
-            'date_of_birth'=> '10.10.2000',
-            'education'=> '',
-        ]);
-        // App\Models\Post::factory()->count(30)->create();
-        // App\Models\Quiz::factory()->count(30)->create();
-        \App\Models\User::factory()->count(200)->create();
-
-        // \App\Models\Quiz::factory()->count(30)->create();
-        Quiz::factory(10)->has(
-            Question::factory(3)
-                ->has(Option::factory(4))
-        )->create();
-        \App\Models\Post::factory()->count(10)->create();
-        // \App\Models\Question::factory()->count(10)->create(['quiz_id'=>1]);
-
-
-        // factory(App\Model\User::class, 50)->create();
-        // factory(App\Model\Post::class, 50)->create();
-        // factory(App\Models\Post::class, 50)->create();
-        // factory(App\Models\Quiz::class, 50)->create();
     }
 }
